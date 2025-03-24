4af973ef0892193ea01e9602ba2819b60ca1ea5a
Igor Khvan <i2gor87@gmail.com>
2025-03-24 19:40:42 +0000

    fix miniconda install script

--- runpod.sh
+++ runpod.sh
@@ -1,4 +1,6 @@
 apt update && apt install vim git-lfs -y
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -p -b
-source ~/anaconda3/bin/activate
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+source ~/miniconda3/bin/activate
